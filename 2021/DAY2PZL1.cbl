21c2f9f5810323bce541ca017a03997ff739c554
Johan Sörlin <johan.sorlin@hotmail.com>
2021-12-02 12:36:27 +0100

    Add COBOL solution for day 2 puzzle 1

---
+++ 2021/DAY2PZL1.cbl
@@ -0,0 +1,72 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. DAY2PZL1.
+
+       ENVIRONMENT DIVISION. 
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+           SELECT PZLINPUT ASSIGN TO 'PZLINPUT.txt'
+           ORGANIZATION IS LINE SEQUENTIAL
+           FILE STATUS IS PZLINPUT-FS.
+       
+       DATA DIVISION.
+       FILE SECTION.
+       FD  PZLINPUT.
+       01  PZL-RECORD              PIC X(15).
+
+       WORKING-STORAGE SECTION.
+       01  WORKSPACE.                             
+           05 PZLINPUT-FS          PIC XX.
+           05 CMD                  PIC X(7).
+           05 NUM                  PIC 9(5).
+           05 POS-X                PIC 9(5).
+           05 NUM-STR              PIC X(5).
+           05 DEPTH                PIC 9(5).
+           05 PRDCT                PIC 9(8).
+       01  SWITCHES.
+           05 PZLINPUT-FLAG        PIC X     VALUE 'C'.
+              88 PZLINPUT-EOF                VALUE 'E'.
+
+       PROCEDURE DIVISION.
+
+       MAIN SECTION.
+           INITIALIZE WORKSPACE
+                      SWITCHES
+
+           OPEN INPUT PZLINPUT
+           PERFORM READ-PZLINPUT
+           PERFORM UNTIL PZLINPUT-EOF
+             PERFORM UNPACK-PZLINPUT
+             PERFORM PROCESS-PZLINPUT
+           END-PERFORM
+           CLOSE PZLINPUT
+
+           MULTIPLY POS-X BY DEPTH GIVING PRDCT
+           DISPLAY PRDCT
+           GOBACK
+           .
+
+       PROCESS-PZLINPUT SECTION.
+           COMPUTE NUM = FUNCTION NUMVAL(NUM-STR)
+           EVALUATE CMD
+             WHEN 'forward'
+               ADD NUM TO POS-X
+             WHEN 'down'
+               ADD NUM TO DEPTH
+             WHEN 'up'
+               SUBTRACT NUM FROM DEPTH
+           END-EVALUATE
+           PERFORM READ-PZLINPUT
+           .
+
+       UNPACK-PZLINPUT SECTION.
+           UNSTRING PZL-RECORD DELIMITED BY ALL spaces
+               INTO CMD
+                    NUM-STR
+           .
+
+       READ-PZLINPUT SECTION.
+           READ PZLINPUT INTO PZL-RECORD
+           IF PZLINPUT-FS NOT = '00'
+             SET PZLINPUT-EOF TO TRUE
+           END-IF
+           .
